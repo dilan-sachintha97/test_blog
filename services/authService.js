0327c168749cad854300f52c24287e00be926066
dilanbotcalm <dilan@botcalm.com>
2024-12-12 19:21:25 +0530

    updated

--- services/authService.js
+++ services/authService.js
@@ -2,10 +2,19 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const { generateToken } = require('../config/jwt');
 
+const validatePassword = (password) => {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+  if (!passwordRegex.test(password)) {
+    throw new Error('Password must be at least 8 characters long and include at least one letter and one number.');
+  }
+};
+
 const registerUser = async (username, password, role) => {
+  validatePassword(password);
   const hashedPassword = await bcrypt.hash(password, 10);
   return User.create({ username, password: hashedPassword, role });
 };
+
 
 const loginUser = async (username, password) => {
   const user = await User.findOne({ where: { username } });
@@ -15,7 +24,5 @@ const loginUser = async (username, password) => {
   const token = generateToken({ id: user.id, role: user.role });
   return { token, user };
 };
-
-
 
 module.exports = { registerUser, loginUser };
